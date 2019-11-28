36d0e482983da8ee05f1fb52be61a402c8d35c74
lrog <lucas.roguski@gmail.com>
2019-11-28 17:43:26 +0000

    further modularizing + cleanup

--- app/src/main/java/app/ServiceApplication.java
+++ app/src/main/java/app/ServiceApplication.java
@@ -2,8 +2,12 @@ package app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@Import({nlp.service.controller.ServiceController.class,
+		nlp.service.config.ApplicationConfiguration.class})
+
 public class ServiceApplication {
 
 	public static void main(String[] args) {
